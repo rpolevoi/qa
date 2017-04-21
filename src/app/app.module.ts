5bed7251f3d0a0738ec45d8b217180eecab9607a
Robert Polevoi <robert.polevoi@gmail.com>
2017-04-21 21:28:40 +0000

    material design added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { routes } from './routes';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DisplayComponent } from './display/display.component';
 import { QAService } from './qa.service';
+import 'hammerjs';
+
 
 
 @NgModule({
@@ -19,8 +23,10 @@ import { QAService } from './qa.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
     RouterModule.forRoot(routes, { useHash: false }),
   ],
   providers: [QAService],
